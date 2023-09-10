30d01f6d31fd7a3258495fdcc0c1572c77b351e7
ahmed osman <ahmedos099912@gmail.com>
2023-09-10 11:05:56 +0200

    moved to components

--- lib/presentation/components/home_components.dart
+++ lib/presentation/components/home_components.dart
@@ -22,3 +22,188 @@ Widget whiteContainer(String type) {
     ),
   );
 }
+
+Widget card(BuildContext context) {
+  return Column(
+    children: [
+      Stack(
+        children: [
+          Positioned(
+            child: Container(
+                width: MediaQuery.sizeOf(context).width,
+                height: MediaQuery.sizeOf(context).height * 0.3,
+                child: FittedBox(
+                    fit: BoxFit.fill,
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Image.asset('assets/imgs/img1.jpg')))),
+          ),
+          Positioned(
+              top: 10,
+              right: 10,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.blue[300],
+                    borderRadius: BorderRadius.circular(50)),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+                  child: Text(
+                    'Buy',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              )),
+          Positioned(
+              bottom: 10,
+              left: 10,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.green[500],
+                    borderRadius: BorderRadius.circular(50)),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.timer_sharp,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        '0-3 Month',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  ),
+                ),
+              )),
+        ],
+      ),
+      SizedBox(
+        height: 5,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Column(
+            children: [
+              Text(
+                'The Shoppes',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.location_on,
+                    color: Colors.grey,
+                  ),
+                  Text(
+                    'Hawaii 81063',
+                    style: TextStyle(color: Colors.grey, fontSize: 14),
+                  )
+                ],
+              )
+            ],
+          ),
+          Container(
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(50)),
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(Icons.favorite_border),
+            ),
+          )
+        ],
+      ),
+      const SizedBox(
+        height: 5,
+      ),
+      const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right: 8.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.bed,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      '5',
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 8.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.bathtub, color: Colors.grey),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      '3',
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 8.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.car_repair, color: Colors.grey),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      '3',
+                      style: TextStyle(color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    '|',
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'House',
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              )
+            ],
+          ),
+          Text(
+            r"$2.1M",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
+    ],
+  );
+}
