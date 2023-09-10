0e7ee9bf6fc4c648c485dafb13e0f9139b54b05a
ahmed osman <ahmedos099912@gmail.com>
2023-09-10 11:02:32 +0200

    first components

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -10,7 +10,7 @@ class Home extends StatelessWidget {
       child: Scaffold(
         body: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.only(top: 20, left: 10, right: 10),
+            padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
             child: Column(
               children: [
                 const Row(
@@ -35,7 +35,7 @@ class Home extends StatelessWidget {
                 Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
-                    color: const Color.fromARGB(255, 11, 107, 186),
+                    color: Color.fromARGB(255, 9, 82, 142),
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
@@ -58,9 +58,219 @@ class Home extends StatelessWidget {
                       ],
                     ),
                   ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Top Properties for you',
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    ),
+                    Text('See all',
+                        style: TextStyle(fontSize: 16, color: Colors.blue)),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Positioned(
+                            child: Container(
+                                width: MediaQuery.sizeOf(context).width,
+                                height: MediaQuery.sizeOf(context).height * 0.3,
+                                child: FittedBox(
+                                    fit: BoxFit.fill,
+                                    child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(50),
+                                        child: Image.asset(
+                                            'assets/imgs/img1.jpg')))),
+                          ),
+                          Positioned(
+                              top: 10,
+                              right: 10,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.blue[300],
+                                    borderRadius: BorderRadius.circular(50)),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 6, horizontal: 12),
+                                  child: Text(
+                                    'Buy',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ),
+                              )),
+                          Positioned(
+                              bottom: 10,
+                              left: 10,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.green[500],
+                                    borderRadius: BorderRadius.circular(50)),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 6, horizontal: 12),
+                                  child: Row(
+                                    children: [
+                                      Icon(
+                                        Icons.timer_sharp,
+                                        color: Colors.white,
+                                      ),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      Text(
+                                        '0-3 Month',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w600),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              )),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'The Shoppes',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.location_on,
+                                    color: Colors.grey,
+                                  ),
+                                  Text(
+                                    'Hawaii 81063',
+                                    style: TextStyle(
+                                        color: Colors.grey, fontSize: 14),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey),
+                                borderRadius: BorderRadius.circular(50)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Icon(Icons.favorite_border),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 8.0),
+                                  child: Row(
+                                    children: [
+                                      Icon(
+                                        Icons.bed,
+                                        color: Colors.grey,
+                                      ),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      Text(
+                                        '5',
+                                        style: TextStyle(color: Colors.grey),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 8.0),
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.bathtub, color: Colors.grey),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      Text(
+                                        '3',
+                                        style: TextStyle(color: Colors.grey),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 8.0),
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.car_repair,
+                                          color: Colors.grey),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      Text(
+                                        '3',
+                                        style: TextStyle(color: Colors.grey),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      '|',
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(
+                                      'House',
+                                      style: TextStyle(color: Colors.grey),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          Text(
+                            r"$2.1M",
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 )
               ],
-            ),
+            ), //The Main column
           ),
         ),
       ),
